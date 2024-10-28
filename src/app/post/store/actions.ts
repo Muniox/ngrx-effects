d5c1e90a5472bcbc72e8419ea3b62ca027413e13
Muniox <muniox20@gmail.com>
2024-10-28 22:28:51 +0100

    Change from createAction to createActionGroup

--- src/app/post/store/actions.ts
+++ src/app/post/store/actions.ts
@@ -1,14 +1,23 @@
-import { createAction, props } from '@ngrx/store';
+import { createActionGroup, emptyProps, props} from '@ngrx/store';
 import { PostInterface } from '../types/posts.interface';
 
-export const getPosts = createAction('[Posts] Get Posts');
+// export const getPosts = createAction('[Posts] Get Posts');
+//
+// export const getPostsSuccess = createAction(
+//   '[Posts] Get Posts Success',
+//   props<{ posts: PostInterface[] }>()
+// );
+//
+// export const getPostsFailure = createAction(
+//   '[Posts] Get Posts Failure',
+//   props<{ error: string }>()
+// );
 
-export const getPostsSuccess = createAction(
-  '[Posts] Get Posts Success',
-  props<{ posts: PostInterface[] }>()
-);
-
-export const getPostsFailure = createAction(
-  '[Posts] Get Posts Failure',
-  props<{ error: string }>()
-);
+export const PostsActions = createActionGroup({
+  source: '[Posts]',
+  events: {
+    'Get Posts': emptyProps(),
+    'Get Posts Success': props<{ posts: PostInterface[] }>(),
+    'Get Posts Failure': props<{ error: string }>(),
+  }
+})
